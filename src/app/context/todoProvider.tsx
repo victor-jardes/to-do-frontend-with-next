69ab523ee66913c402024083ae6d3c087d543ce6
victor jardes <victor_maanoel@outlook.com>
2023-05-13 22:04:10 -0400

    feat(todoprovider.tsx): add news
    add `InputValueAttribute` type for tiping params in `finishedTodo` funtion and remove inital values
    in `todo` state and add fun
    
    BREAKING CHANGE: add function for set `isFinished` attr in `Itodo` type

--- src/app/context/todoProvider.tsx
+++ src/app/context/todoProvider.tsx
@@ -4,21 +4,33 @@ import { ITodo } from "@/@types/todo";
 import { TodoContext } from "./todoContext";
 import { useState } from "react";
 import { IComponentChildren } from "@/@types/IComponentChildren";
+import { InputValueAttibute } from "@/@types/IInputValueAttribute";
 
 export default function TodoProvider({ children }: IComponentChildren) {
-  const [todos, setAllTodos] = useState<ITodo[]>([{ id: "", description: "" }]);
+  const [todos, setAllTodos] = useState<ITodo[]>([]);
 
   const saveTodo = ({ description }: ITodo) => {
     const newTodo: ITodo = {
       id: self.crypto.randomUUID(),
       description,
+      isFinished: false,
     };
     setAllTodos([...todos, newTodo]);
+  };
+
+  const finishedTodo = ({ target }: InputValueAttibute) => {
+    todos.filter((todo: ITodo) => {
+      if (todo.id === target.value) {
+        todo.isFinished = !todo.isFinished;
+        setAllTodos([...todos]);
+      }
+    });
   };
 
   const values = {
     todos,
     saveTodo,
+    finishedTodo,
   };
   return <TodoContext.Provider value={values}>{children}</TodoContext.Provider>;
 }

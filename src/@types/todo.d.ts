6041f9aabe5ad3e148d7cfae03cfaf63f6ce8af5
victor jardes <victor_maanoel@outlook.com>
2023-05-13 21:50:48 -0400

    refactor(@types/todo.d.ts): attributes
    add `isFinished` attr in `Itodo` interface and add function `finishedTodo` for set `isFinishedAttr`
    for true
    
    BREAKING CHANGE: add new attr `isFinished` in Itodo type

--- src/@types/todo.d.ts
+++ src/@types/todo.d.ts
@@ -1,9 +1,13 @@
+import { IButtonValueAttribute } from "./IButtonValueAttribute";
+
 export interface ITodo {
   id: string;
   description: string;
+  isFinished: boolean;
 }
 
 export type TodoContextType = {
   todos: ITodo[];
   saveTodo: ({ description }: partial<ITodo>) => void;
+  finishedTodo: ({ target }: IButtonValueAttribute) => void;
 };

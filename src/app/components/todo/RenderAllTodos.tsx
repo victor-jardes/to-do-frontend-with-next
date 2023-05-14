b54778732829759d3e320c68340364bfa2eec777
victor jardes <victor_maanoel@outlook.com>
2023-05-13 21:58:44 -0400

    refactor(renderalltodos.tsx): change function and tags HTML in `component`
    but add simple initials `functions` and simple `busines rules`

--- src/app/components/todo/RenderAllTodos.tsx
+++ src/app/components/todo/RenderAllTodos.tsx
@@ -1,19 +1,41 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TodoContext } from "../../context/todoContext";
 import { TodoContextType } from "@/@types/todo";
+import { IButtonValueAttribute } from "@/@types/IButtonValueAttribute";
+
+const isEmptyValue = () => {
+  return <p>empty...</p>;
+};
 
 export default function RenderALlTodos() {
-  const { todos } = useContext(TodoContext) as TodoContextType;
+  const { todos, finishedTodo } = useContext(TodoContext) as TodoContextType;
+
   return (
     <>
       <ul>
-        {Object.keys(todos[0]).length <= 0 ? (
-          <p>empty...</p>
-        ) : (
-          todos.map(({ id, description }) => <li key={id}>{description}</li>)
-        )}
+        {todos.length <= 0
+          ? isEmptyValue()
+          : todos.map(({ id, description, isFinished }) => (
+              <li
+                key={id}
+                style={
+                  isFinished
+                    ? { backgroundColor: "red" }
+                    : { backgroundColor: "inherit" }
+                }
+              >
+                {description}
+                <button
+                  type="button"
+                  value={id}
+                  onClick={(e: IButtonValueAttribute) => finishedTodo(e)}
+                >
+                  complet
+                </button>
+              </li>
+            ))}
       </ul>
     </>
   );

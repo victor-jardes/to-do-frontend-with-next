94d4ed89ccd1236292032139e4228ae8aa206a84
victor jardes <victor_maanoel@outlook.com>
2023-05-24 17:21:58 -0400

    refactor(inputstodo.tsx): rafactor for using in test component
    add import for type, create interface for type props....

--- src/app/inputsTodo/InputsTodo.tsx
+++ src/app/inputsTodo/InputsTodo.tsx
@@ -1,47 +1,63 @@
-import { Dispatch } from "react";
-import { ITodo } from "@/@types/todo";
+"use client";
+import React, { useContext } from "react";
 import { InputValueAttibute } from "@/@types/IInputValueAttribute";
+import { TodoContext } from "../context/todoContext";
+import { TodoContextType } from "@/@types/ITodo";
 
-interface propsType {
+type InputWriteTodoProps = {
   taskValue: string;
-  setTaskValue: Dispatch<string>;
-  saveTodo: ({ description }: Partial<ITodo>) => void;
+  handleWriteTask: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const InputWriteTodo = ({
+  handleWriteTask,
+  taskValue,
+}: InputWriteTodoProps) => {
+  return (
+    <input
+      type="text"
+      placeholder="Write your task here"
+      onChange={(e) => handleWriteTask(e)}
+      value={taskValue}
+      data-testid="data-test-write-todo"
+    />
+  );
+};
+
+interface ButtonAddTodoProps {
+  taskValue: string;
+  handleAddTask: () => void;
 }
 
-export default function InputsTodo({
-  taskValue,
-  setTaskValue,
-  saveTodo,
-}: propsType) {
+const ButtonAddTodo = ({ handleAddTask, taskValue }: ButtonAddTodoProps) => {
+  return (
+    <input
+      type="button"
+      value="ADD"
+      onClick={() => handleAddTask()}
+      disabled={taskValue.length >= 2 ? false : true}
+      data-testid="data-test-button-add-todo"
+    />
+  );
+};
+
+export default function InputsTodo() {
+  const { saveTodo, setTaskValue, taskValue } = useContext(
+    TodoContext
+  ) as TodoContextType;
   const handleWriteTask = ({ target }: InputValueAttibute) => {
     setTaskValue(target.value);
   };
 
-  const handleAddTask = (description: string) => {
-    if (typeof saveTodo === "function") {
-      saveTodo({ description });
-      setTaskValue("");
-    } else {
-      return console.log("save todo is not a function");
-    }
+  const handleAddTask = () => {
+    saveTodo();
+    setTaskValue("");
   };
 
   return (
     <>
-      <input
-        type="text"
-        placeholder="Write your task here"
-        onChange={(e) => handleWriteTask(e)}
-        value={taskValue}
-        data-testid="data-test-write-todo"
-      />
-      <input
-        type="button"
-        value="ADD"
-        onClick={() => handleAddTask(taskValue)}
-        disabled={taskValue.length >= 2 ? false : true}
-        data-testid="data-test-button-add-todo"
-      />
+      <InputWriteTodo handleWriteTask={handleWriteTask} taskValue={taskValue} />
+      <ButtonAddTodo handleAddTask={handleAddTask} taskValue={taskValue} />
     </>
   );
 }

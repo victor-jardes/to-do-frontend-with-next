98a1b095a2e7337d8a03375f451fe686452f3a42
victor jardes <victor_maanoel@outlook.com>
2023-05-26 08:56:34 -0400

    test(renderalltodos): refactor test
    refactor everythin  in test

--- src/app/renderTodo/RenderAllTodos.cy.tsx
+++ src/app/renderTodo/RenderAllTodos.cy.tsx
@@ -2,6 +2,24 @@ import React from "react";
 import RenderALlTodos from "./RenderAllTodos";
 import { ITodo } from "@/@types/ITodo";
 
+const MY_ALL_TODOS = [
+  {
+    id: "1",
+    description: "wash to car",
+    isFinished: false,
+  },
+  {
+    id: "2",
+    description: "study English",
+    isFinished: false,
+  },
+  {
+    id: "3",
+    description: "clear my room",
+    isFinished: true,
+  },
+];
+
 describe("<RenderALlTodos /> components", () => {
   it("should be able rendering `empty` conditional function containing `<p>` tag HTML", () => {
     const EMPTY_VALUE_RENDER = "[data-testid='data-test-empty-value']";
@@ -11,44 +29,57 @@ describe("<RenderALlTodos /> components", () => {
   });
 
   it("should be able render `<ul>` tag and `<li>` with `<button>` element in list", () => {
-    const myValues: ITodo[] = [];
+    const myValues: ITodo[] = MY_ALL_TODOS;
     const DATA_TESTID_LIST_WITH_TODOS =
-      "[datat-testid='data-test-list-with-todos']";
+      "[data-testid='data-test-list-with-todos']";
 
-    cy.fixture("myTodosExemple.json").then((todo: ITodo[]) => {
-      for (let i = 0; i < todo.length; i += 1) {
-        myValues.push(todo[i]);
-      }
-      cy.mountTodoApp(<RenderALlTodos todos={myValues} />);
+    cy.mountTodoApp(<RenderALlTodos todos={myValues} />);
 
-      cy.get(DATA_TESTID_LIST_WITH_TODOS)
-        .should("be.visible")
-        .and(($el) => {
-          expect($el[0].tagName).to.equal("UL");
-        })
-        .children("li")
-        .and("have.length", myValues.length);
+    cy.get(DATA_TESTID_LIST_WITH_TODOS)
+      .should("be.visible")
+      .then(($el) => {
+        expect($el.children("li").length).to.equal(3);
+        return $el.children("li");
+      })
+      .as("allLis");
 
-      cy.get(DATA_TESTID_LIST_WITH_TODOS)
-        .should("be.visible")
-        .children()
-        .each(($el) => {
-          const DATA_TESTID_ITEM_IN_LIST = `[data-testid='data-test-item-in-list-${$el.attr(
-            "id"
-          )}']`;
-          const DATA_TESTID_BUTTON_COMPLET_TODO = `[data-testid='data-test-button-complet-${$el.attr(
-            "id"
-          )}']`;
-          cy.get(DATA_TESTID_ITEM_IN_LIST)
-            .should("be.visible")
-            .and("have.attr", "id")
-            .and("equal", $el.attr("id"));
+    cy.get("@allLis").and(($el) => {
+      expect($el[0].textContent).to.equal("wash to car");
+      expect($el[0].id).to.equal("1");
+      expect($el[0].style.backgroundColor).to.equal("inherit");
 
-          cy.get(DATA_TESTID_BUTTON_COMPLET_TODO)
-            .should("be.visible")
-            .and("have.value", $el.attr("id"))
-            .and("have.text", "COMPLET");
-        });
+      expect($el[1].textContent).to.equal("study English");
+      expect($el[1].id).to.equal("2");
+      expect($el[1].style.backgroundColor).to.equal("inherit");
+
+      expect($el[2].textContent).to.equal("clear my room");
+      expect($el[2].id).to.equal("3");
+      expect($el[2].style.backgroundColor).to.equal("red");
     });
-  });
-});
+
+    cy.get(DATA_TESTID_LIST_WITH_TODOS)
+      .should("be.visible")
+      .then(($el) => {
+        expect($el.children("button").length).to.equal(3);
+        return $el.children("button");
+      })
+      .as("allButtons");
+
+    cy.get("@allButtons").and(($el) => {
+      console.log($el[0]);
+
+      const btnComple = "button-complet";
+      expect($el[0].id).to.equal(`${btnComple}-1`);
+      expect($el[0].getAttribute("value")).to.equal("1");
+      expect($el[0].textContent).to.equal("COMPLET");
+
+      expect($el[1].id).to.equal(`${btnComple}-2`);
+      expect($el[1].getAttribute("value")).to.equal("2");
+      expect($el[1].textContent).to.equal("COMPLET");
+
+      expect($el[2].id).to.equal(`${btnComple}-3`);
+      expect($el[2].getAttribute("value")).to.equal("3");
+      expect($el[2].textContent).to.equal("COMPLET");
+    });
+  })
+})

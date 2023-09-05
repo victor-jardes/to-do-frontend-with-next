dcbb10309750bae9678fc03f16d6185c43372aca
victor <victor_maanoel@outlook.com>
2023-09-04 21:37:30 -0400

    refactor(globals style): change file for use `styled-components`

---
+++ src/app/globals.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const styled = { createGlobalStyle };
+
+export const GlobalStyle = styled.createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  body {
+    font-weight: normal;
+    margin: 0;
+    padding: 0;
+  }
+`;

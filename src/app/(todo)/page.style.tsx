56fa16dd57b774691f2ab9904ae3b2843d6f8071
victor <victor_maanoel@outlook.com>
2023-09-08 20:10:21 -0400

    feat(page.style.tsx): add css

--- src/app/(todo)/page.style.tsx
+++ src/app/(todo)/page.style.tsx
@@ -6,4 +6,9 @@ export const StyledMainTodoPage = styled.main`
   background-color: red;
   display: flex;
   flex-direction: column;
+  height: 50vh;
+  margin: auto;
+  min-height: 50vh;
+  min-width: 80%;
+  width: 85%;
 `;

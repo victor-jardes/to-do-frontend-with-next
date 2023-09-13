7109e561da92436adee8546b08b08acea3185cdd
victor <victor_maanoel@outlook.com>
2023-09-13 16:12:20 -0400

    feat(renderalltodos.style.tsx): create file for style in list
    add firt style propertie for simple style and export const wiht all style for use in comoponent

---
+++ src/app/(todo)/components/list/renderAllTodos.style.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const BlockWithListOfAllTodos = styled.div`
+  border-color: black;
+  border-style: solid;
+  height: 95%;
+  margin: auto;
+  width: 98%;
+  overflow: hidden;
+  border-radius: 0.4rem;
+`;

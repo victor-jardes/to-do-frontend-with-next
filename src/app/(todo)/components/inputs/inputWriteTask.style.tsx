2a22923587ed2f4c0a32c3db67a90bd9197ab332
victor <victor_maanoel@outlook.com>
2023-09-12 20:58:46 -0400

    refactor(inputwritetask.style.css): add and change css
    remove `label` const, and add more css in `input`

--- src/app/(todo)/components/inputs/inputWriteTask.style.tsx
+++ src/app/(todo)/components/inputs/inputWriteTask.style.tsx
@@ -2,4 +2,17 @@
 
 import styled from "styled-components";
 
-export const StyledInputTodoWriteTask = styled.input``;
+export const StyledInputTodoWriteTask = styled.input`
+  border-style: none;
+  font-size: 2rem;
+  height: 15%;
+  min-height: 12%;
+  min-width: 100%;
+  width: 100%;
+
+  &:hover {
+    border-radius: 0.2rem;
+    border-style: none;
+    box-shadow: 0 0 2px 1px green;
+  }
+`;

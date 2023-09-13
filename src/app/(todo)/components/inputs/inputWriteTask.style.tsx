26ffea7c25c7a445fc7a5ae439993d5a2457fde3
victor <victor_maanoel@outlook.com>
2023-09-13 01:42:15 -0400

    refactor(inputwrite css): add `margim-bottom` for more space and change `bg-color` and add outline

--- src/app/(todo)/components/inputs/inputWriteTask.style.tsx
+++ src/app/(todo)/components/inputs/inputWriteTask.style.tsx
@@ -6,13 +6,16 @@ export const StyledInputTodoWriteTask = styled.input`
   border-style: none;
   font-size: 2rem;
   height: 15%;
+  margin-bottom: 0.9rem;
   min-height: 12%;
   min-width: 100%;
   width: 100%;
 
-  &:hover {
+  &:hover,
+  &:focus {
+    background-color: rgba(255, 255, 255, 0.973);
     border-radius: 0.2rem;
-    border-style: none;
-    box-shadow: 0 0 2px 1px green;
+    outline: 0.2rem solid #e6f7ef;
+    outline-offset: 0.1rem;
   }
 `;

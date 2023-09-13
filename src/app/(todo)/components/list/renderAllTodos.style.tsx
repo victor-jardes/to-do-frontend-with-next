da4b7dffaff9e0e3fc6f0395d0628768d5e69404
victor <victor_maanoel@outlook.com>
2023-09-13 17:32:25 -0400

    feat(list css): add css for cutomize item in list

--- src/app/(todo)/components/list/renderAllTodos.style.tsx
+++ src/app/(todo)/components/list/renderAllTodos.style.tsx
@@ -6,6 +6,20 @@ export const BlockWithListOfAllTodos = styled.div`
   height: 95%;
   margin: auto;
   width: 98%;
-  overflow: hidden;
   border-radius: 0.4rem;
+`;
+
+export const ListWraper = styled.div`
+  display: grid;
+  gap: 1.6rem;
+  grid-template-columns: 1fr auto;
+`;
+
+export const TextContainer = styled.div`
+  flex-grow: 1;
+`;
+
+export const StyledListItemWithTask = styled.li`
+  font-size: 1.6rem;
+  white-space: normal;
 `;

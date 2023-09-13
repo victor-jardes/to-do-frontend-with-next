7f2ae478d90351d00de99662780eb3d13044196b
victor <victor_maanoel@outlook.com>
2023-09-12 23:00:57 -0400

    feat(css): add css in `button` component

--- src/app/(todo)/components/inputs/buttonAddTask.style.tsx
+++ src/app/(todo)/components/inputs/buttonAddTask.style.tsx
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
-export const StyledButtonTodoForAddTask = styled.button``;
+export const StyledButtonTodoForAddTask = styled.button`
+  background-color: gray;
+  cursor: not-allowed;
+  font: white;
+  opacity: 0.5;
+
+  &:enabled {
+    background-color: white;
+    border-radius: 0.3rem;
+    border-style: none;
+    box-shadow: 0 0 0.1rem 0.2rem #b6eeae;
+    cursor: pointer;
+    font: black;
+    padding: 0.3rem;
+    opacity: 0.9;
+
+    &:hover {
+      background-color: #dfe4f0f6;
+      box-shadow: 0 0 0.1rem 0.2rem #78d2f3;
+    }
+  }
+`;

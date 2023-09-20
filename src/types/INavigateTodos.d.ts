4b953196054fc22e04931add80ac5fbd27e55d07
victor <victor_maanoel@outlook.com>
2023-09-20 17:05:18 -0400

    refactor(interface names): change for add `indexTodo` in world for type name

--- src/types/INavigateTodos.d.ts
+++ src/types/INavigateTodos.d.ts
@@ -1,16 +1,16 @@
-export interface INavigateTodo {
+export interface INavigateIndexTodo {
   currentPage: number;
   todos: ITodo[];
   itemsPerPage: number;
   setCurrentPage: Dispatch<SetStateAction<number>>;
 }
 
-export interface IGoTonextPageParams extends INavigateTodo {}
+export interface IGoToNextIndexTodoPageParams extends INavigateIndexTodo {}
 
-export interface IGoToPreviousPageParams
-  extends Pick<INavigateTodo, "currentPage" | "setCurrentPage"> {}
+export interface IGoToPreviousIndexTodoPageParams
+  extends Pick<INavigateIndexTodo, "currentPage" | "setCurrentPage"> {}
 
-export interface IGoToPageParams
-  extends Pick<INavigateTodo, "currentPage" | "setCurrentPage"> {
+export interface IGoToIndexTodoPageParams
+  extends Pick<INavigateIndexTodo, "currentPage" | "setCurrentPage"> {
   pageIndex: number;
 }

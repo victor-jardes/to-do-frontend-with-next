90a2967037aafc801274771034ac46572b573072
victor <victor_maanoel@outlook.com>
2023-09-20 20:51:45 -0400

    feat(renderalltodos.tsx): add `states` for `Navigate` component add change `constant` of `map`
    change `constant` for `immutability` in render

--- src/app/(todo)/components/list/RenderAllTodos.tsx
+++ src/app/(todo)/components/list/RenderAllTodos.tsx
@@ -2,13 +2,14 @@
 
 import { ITodo, TodoContextType } from "@/types/ITodo";
 import { useGetContextForList } from "./utils/useGetContextForList";
-import { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import {
   BlockWithListOfAllTodos,
   StyledListItemWithTask,
   ListWraper,
   TextContainer,
 } from "./renderAllTodos.style";
+import { NavigateTodo } from "./navigateTodo/NavigateTodo";
 
 export function RenderEmpty() {
   return (
@@ -54,30 +55,48 @@ export function ListWithtodos({
 export default function RenderAllTodos() {
   const { finishedTodo, todos } = useGetContextForList();
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState<number>(5);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const currentItems = todos.slice(startIndex, endIndex);
+
   return (
     <>
-      {todos.length <= 0 ? (
-        <RenderEmpty />
-      ) : (
-        <BlockWithListOfAllTodos>
-          <ul
-            id="list-with-todos"
-            role="list"
-            data-testid="data-test-list-with-todos"
-          >
-            {todos.map(({ id, description, isFinished }) => (
-              <Fragment key={id}>
-                <ListWithtodos
-                  id={id}
-                  isFinished={isFinished}
-                  description={description}
-                  finishedTodo={finishedTodo}
-                />
-              </Fragment>
-            ))}
-          </ul>
-        </BlockWithListOfAllTodos>
-      )}
+      <div>
+        {todos.length <= 0 ? (
+          <RenderEmpty />
+        ) : (
+          <BlockWithListOfAllTodos>
+            <ul
+              id="list-with-todos"
+              role="list"
+              data-testid="data-test-list-with-todos"
+            >
+              {currentItems.map(({ id, description, isFinished }) => (
+                <Fragment key={id}>
+                  <ListWithtodos
+                    id={id}
+                    isFinished={isFinished}
+                    description={description}
+                    finishedTodo={finishedTodo}
+                  />
+                </Fragment>
+              ))}
+            </ul>
+          </BlockWithListOfAllTodos>
+        )}
+      </div>
+      <div>
+        <NavigateTodo
+          currentPage={currentPage}
+          itemsPerPage={itemsPerPage}
+          setCurrentPage={setCurrentPage}
+          todos={todos}
+        />
+      </div>
     </>
   );
 }

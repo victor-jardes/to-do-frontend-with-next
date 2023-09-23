46d341ae8364b9fdb0829deaa1e0351627c01bee
victor <victor_maanoel@outlook.com>
2023-09-23 00:05:30 -0400

    refactor(renderalltodos.tsx): a long refactor in component
    refactor for a creation simple component for maintanance in futere and this component will more
    `clean`

--- src/app/(todo)/components/list/RenderAllTodos.tsx
+++ src/app/(todo)/components/list/RenderAllTodos.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { ITodo, TodoContextType } from "@/types/ITodo";
 import { useGetContextForList } from "./utils/useGetContextForList";
-import React, { Fragment, useState } from "react";
-import {
-  BlockWithListOfAllTodos,
-  StyledListItemWithTask,
-  ListWraper,
-  TextContainer,
-} from "./renderAllTodos.style";
+import React, { useState } from "react";
+import { BlockWithListOfAllTodos } from "./renderAllTodos.style";
 import { NavigateTodo } from "./navigateTodo/NavigateTodo";
+import { ListWithTodos } from "./listWithTodos/ListWithTodos";
+import { ItemWithTasks } from "./listWithTodos/components/ItemWithTask.li";
 
 export function RenderEmpty() {
   return (
@@ -21,47 +17,11 @@ export function RenderEmpty() {
   );
 }
 
-export function ListWithtodos({
-  id,
-  isFinished,
-  description,
-  finishedTodo,
-}: ITodo & Pick<TodoContextType, "finishedTodo">) {
-  return (
-    <ListWraper>
-      <TextContainer>
-        <StyledListItemWithTask
-          $isFinished={isFinished}
-          id={id}
-          data-testid={`data-test-item-in-list-${id}`}
-          value={description}
-        >
-          {description}
-        </StyledListItemWithTask>
-      </TextContainer>
-      <button
-        id={`button-complet-${id}`}
-        type="button"
-        value={id}
-        onClick={(element) => finishedTodo(element.currentTarget.value)}
-        data-testid={`data-test-button-complet-${id}`}
-      >
-        COMPLET
-      </button>
-    </ListWraper>
-  );
-}
-
 export default function RenderAllTodos() {
-  const { finishedTodo, todos } = useGetContextForList();
+  const { todos } = useGetContextForList();
 
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState<number>(5);
-
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-
-  const currentItems = todos.slice(startIndex, endIndex);
 
   return (
     <>
@@ -70,22 +30,12 @@ export default function RenderAllTodos() {
           <RenderEmpty />
         ) : (
           <BlockWithListOfAllTodos>
-            <ul
-              id="list-with-todos"
-              role="list"
-              data-testid="data-test-list-with-todos"
-            >
-              {currentItems.map(({ id, description, isFinished }) => (
-                <Fragment key={id}>
-                  <ListWithtodos
-                    id={id}
-                    isFinished={isFinished}
-                    description={description}
-                    finishedTodo={finishedTodo}
-                  />
-                </Fragment>
-              ))}
-            </ul>
+            <ListWithTodos>
+              <ItemWithTasks
+                currentPage={currentPage}
+                itemsPerPage={itemsPerPage}
+              />
+            </ListWithTodos>
           </BlockWithListOfAllTodos>
         )}
       </div>

f1a4f074ef0ec51cd927a110fbdf5619a0f14e6a
victor <victor_maanoel@outlook.com>
2023-10-02 17:38:35 -0400

    refactor(css list): separete files for css more clean

---
+++ src/app/(todo)/components/list/listWithTodos/components/itemWithTask.style.tsx
@@ -0,0 +1,34 @@
+import styled, { css } from "styled-components";
+
+export const ListWraper = styled.div`
+  display: flex;
+  gap: 1.6rem;
+  margin: 0.7rem 0;
+  height: 10%;
+`;
+
+export const TextContainer = styled.div`
+  align-self: center;
+  background-color: rgba(245, 246, 250, 0.979);
+  border-radius: 0.5rem;
+  flex-grow: 1;
+  height: max-content;
+  min-height: max-content;
+`;
+
+const notFinishedTaskStyle = css`
+  font-size: 1.6rem;
+  list-style: none;
+  text-align: justify;
+`;
+
+const finishedTaskStyle = css`
+  ${notFinishedTaskStyle}
+  color: grey;
+  text-decoration: line-through;
+`;
+
+export const StyledListItemWithTask = styled.li<{ $isFinished: boolean }>`
+  white-space: normal;
+  ${(props) => (props.$isFinished ? finishedTaskStyle : notFinishedTaskStyle)}
+`;

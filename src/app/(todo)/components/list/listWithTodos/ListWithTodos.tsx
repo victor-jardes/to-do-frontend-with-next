691a3ed0750f04f8f253e1bbe0dd596770515070
victor <victor_maanoel@outlook.com>
2023-10-03 20:19:49 -0400

    feat(listwithtodos.tsx): add fixed `height`

--- src/app/(todo)/components/list/listWithTodos/ListWithTodos.tsx
+++ src/app/(todo)/components/list/listWithTodos/ListWithTodos.tsx
@@ -4,6 +4,7 @@ export function ListWithTodos({ children }: IComponentChildren) {
   return (
     <>
       <ul
+        style={{ height: "100%" }}
         id="list-with-todos"
         role="list"
         data-testid="data-test-list-with-todos"

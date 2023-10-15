34202f383074731ebccf65b506a9fa51c44daa29
victor <victor_maanoel@outlook.com>
2023-10-14 23:42:45 -0400

    feat(delet file): delete for dont use this `function`

--- src/app/(todo)/components/list/navigateTodo/auxFunctions/goToIndexTodoPageAux.ts
+++
@@ -1,8 +0,0 @@
-import { INavigateIndexTodo } from "@/types/INavigateTodos";
-
-export function goToIndexTodoPageAux(
-  pageIndex: number,
-  { setCurrentPage }: Pick<INavigateIndexTodo, "setCurrentPage">
-) {
-  setCurrentPage(pageIndex);
-}

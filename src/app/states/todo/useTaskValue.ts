e2e68b5d88e532c577a342e291cc4da21a25225f
victor <victor_maanoel@outlook.com>
2023-10-21 18:00:39 -0400

    feat(usetaskvalue.ts): add `Action` for reset a initial state

--- src/app/states/todo/useTaskValue.ts
+++ src/app/states/todo/useTaskValue.ts
@@ -6,11 +6,19 @@ type State = {
 
 type Action = {
   setTaskValue: (taskValue: string) => void;
+  reset: () => void;
+};
+
+const initialState: State = {
+  taskValue: "",
 };
 
 const useTaskValue = create<State & Action>()((set) => ({
   taskValue: "",
   setTaskValue: (taskValue) => set(() => ({ taskValue: taskValue })),
+  reset() {
+    set(initialState);
+  },
 }));
 
 export default useTaskValue;

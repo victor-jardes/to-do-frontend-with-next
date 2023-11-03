48e4316c633d61cb58e27b68eb33f62e2531d5d5
victor <victor_maanoel@outlook.com>
2023-11-03 17:30:00 -0400

    test(cypress/renderalltodos): fix simple errors
    add a new `describe` block for this scope, and call `zustand-state`

--- cypress/tests/unit/app/todo/components/RenderAllTodos.cy.tsx
+++ cypress/tests/unit/app/todo/components/RenderAllTodos.cy.tsx
@@ -5,6 +5,7 @@ import RenderAllTodos from "@/app/(todo)/components/list/RenderAllTodos";
 import { ITodo } from "@/types/ITodo";
 import { rest } from "msw";
 import { mswWorker } from "@/apiMocks/mswWorker";
+import useTodos from "@/app/states/todo/useTodo";
 
 const MY_ALL_TODOS = [
   {
@@ -32,64 +33,68 @@ describe("<RenderALlTodos /> components", () => {
     cy.get(EMPTY_VALUE_RENDER).should("be.visible", true);
   });
 
-  it("should be able render `<ul>` tag and `<li>` with `<button>` element in list", () => {
+  describe("when contain tasks", () => {
     const myValues: ITodo[] = MY_ALL_TODOS;
-    const DATA_TESTID_LIST_WITH_TODOS =
-      "[data-testid='data-test-list-with-todos']";
 
-    mswWorker.use(
-      rest.get("http://localhost:3001/todo/findAll", (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json(myValues));
-      })
-    );
+    beforeEach(() => {
+      mswWorker.use(
+        rest.get("http://localhost:3001/todo/findAll", (req, res, ctx) => {
+          return res(ctx.status(200), ctx.json(myValues));
+        })
+      );
 
-    cy.mountTodoApp(<RenderAllTodos />);
-
-    cy.get(DATA_TESTID_LIST_WITH_TODOS)
-      .should("be.visible")
-      .then(($el) => {
-        expect($el.children("li").length).to.equal(3);
-        return $el.children("li");
-      })
-      .as("allLis");
-
-    cy.get("@allLis").and(($el) => {
-      expect($el[0].textContent).to.equal("wash to car");
-      expect($el[0].id).to.equal("1");
-      expect($el[0].style.backgroundColor).to.equal("inherit");
-
-      expect($el[1].textContent).to.equal("study English");
-      expect($el[1].id).to.equal("2");
-      expect($el[1].style.backgroundColor).to.equal("inherit");
-
-      expect($el[2].textContent).to.equal("clear my room");
-      expect($el[2].id).to.equal("3");
-      expect($el[2].style.backgroundColor).to.equal("red");
+      useTodos.getState().fetchAllTodos();
     });
+    it("should be able render `<ul>` tag and `<li>` with `<button>` element in list", () => {
+      const DATA_TESTID_LIST_WITH_TODOS =
+        "[data-testid='data-test-list-with-todos']";
 
-    cy.get(DATA_TESTID_LIST_WITH_TODOS)
-      .should("be.visible")
-      .then(($el) => {
-        expect($el.children("button").length).to.equal(3);
-        return $el.children("button");
-      })
-      .as("allButtons");
+      cy.mountTodoApp(<RenderAllTodos />);
 
-    cy.get("@allButtons").and(($el) => {
-      console.log($el[0]);
+      cy.get(DATA_TESTID_LIST_WITH_TODOS)
+        .should("be.visible")
+        .then(($el) => {
+          expect($el.children("div").children("li").length).to.equal(3);
+          return $el.children("div").children("li");
+        })
+        .as("allLis");
 
-      const btnComple = "button-complet";
-      expect($el[0].id).to.equal(`${btnComple}-1`);
-      expect($el[0].getAttribute("value")).to.equal("1");
-      expect($el[0].textContent).to.equal("COMPLET");
+      cy.get("@allLis").and(($el) => {
+        expect($el[0].textContent).to.equal("wash to car");
+        expect($el[0].id).to.equal("1");
 
-      expect($el[1].id).to.equal(`${btnComple}-2`);
-      expect($el[1].getAttribute("value")).to.equal("2");
-      expect($el[1].textContent).to.equal("COMPLET");
+        expect($el[1].textContent).to.equal("study English");
+        expect($el[1].id).to.equal("2");
 
-      expect($el[2].id).to.equal(`${btnComple}-3`);
-      expect($el[2].getAttribute("value")).to.equal("3");
-      expect($el[2].textContent).to.equal("COMPLET");
+        expect($el[2].textContent).to.equal("clear my room");
+        expect($el[2].id).to.equal("3");
+      });
+
+      cy.get(DATA_TESTID_LIST_WITH_TODOS)
+        .should("be.visible")
+        .then(($el) => {
+          console.log($el.children());
+          expect($el.children("div").children("button").length).to.equal(3);
+          return $el.children("div").children("button");
+        })
+        .as("allButtons");
+
+      cy.get("@allButtons").and(($el) => {
+        console.log($el[0]);
+
+        const btnComple = "button-complet";
+        expect($el[0].id).to.equal(`${btnComple}-1`);
+        expect($el[0].getAttribute("value")).to.equal("1");
+        expect($el[0].textContent).to.equal("COMPLET");
+
+        expect($el[1].id).to.equal(`${btnComple}-2`);
+        expect($el[1].getAttribute("value")).to.equal("2");
+        expect($el[1].textContent).to.equal("COMPLET");
+
+        expect($el[2].id).to.equal(`${btnComple}-3`);
+        expect($el[2].getAttribute("value")).to.equal("3");
+        expect($el[2].textContent).to.equal("COMPLET");
+      });
     });
   });
 });

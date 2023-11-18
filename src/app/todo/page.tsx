b49d6e5322e35fed86782af31c82751fe5e3cf37
victor <victor_maanoel@outlook.com>
2023-11-18 19:06:10 -0400

    build(architecture): change `architecture`

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,6 +1,6 @@
 import InputsTodo from "./components/inputs/InputsTodo";
 import RenderAllTodos from "./components/list/RenderAllTodos";
-import { NavigateTodo } from "./components/list/navigateTodo/NavigateTodo";
+import { NavigateTodo } from "./components/navigateTodo/NavigateTodo";
 import { StyledMainTodoPage } from "./page.style";
 
 export default function Todo() {

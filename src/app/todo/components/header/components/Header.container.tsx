4428764239e6bb3507a88001a121d7fea513366f
victor <victor_maanoel@outlook.com>
2023-11-19 14:23:08 -0400

    feat(header): create a initial header for `todo` page

---
+++ src/app/todo/components/header/components/Header.container.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { StyledHeaderContainer } from "../styles/header.container.styles";
+
+export default function HeaderContainer() {
+  return (
+    <StyledHeaderContainer>
+      <p>HEADER</p>
+    </StyledHeaderContainer>
+  );
+}

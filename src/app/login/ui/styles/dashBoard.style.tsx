c1d6e09c56025e85636c423d8a15cf37f33a9f18
victor <victor_maanoel@outlook.com>
2023-11-19 21:41:17 -0400

    style(dashboard): use `css grid` now

--- src/app/login/ui/styles/dashBoard.style.tsx
+++ src/app/login/ui/styles/dashBoard.style.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const StyledDashBoard = styled.div`
+  display: grid;
   background: #f0f0f0;
   border-radius: 10px;
   border-color: black;
   border-style: solid;
   flex-shrink: 0;
+  grid-template-columns: repeat(2, 1fr);
   height: 500px;
   margin: auto;
   width: 900px;

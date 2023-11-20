c6ece7e10c84ecdeb59546f251b7f950878e2eaa
victor <victor_maanoel@outlook.com>
2023-11-19 21:39:17 -0400

    style(useinfoscontainer): use `css grid` now

--- src/app/login/ui/styles/userInfosContainer.style.tsx
+++ src/app/login/ui/styles/userInfosContainer.style.tsx
@@ -1,11 +1,9 @@
 import styled from "styled-components";
 
 export const StyledUserInfosContainer = styled.div`
-  align-items: center;
-  background: #fff;
-  display: flex;
-  flex-direction: column;
+  background-color: #fff;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 4;
   height: 467px;
-  justify-content: center;
-  width: 534px;
 `;

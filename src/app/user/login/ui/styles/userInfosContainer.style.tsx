bc12489967df0bfdca61e15392bcc15771cfbf3a
victor <victor_maanoel@outlook.com>
2023-12-27 20:17:03 -0400

    refactor(userinfocontainer): add fix value for `grid`

--- src/app/user/login/ui/styles/userInfosContainer.style.tsx
+++ src/app/user/login/ui/styles/userInfosContainer.style.tsx
@@ -7,6 +7,7 @@ export const StyledUserInfosContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 4;
+  grid-template-rows: 50px 100px 200px auto;
   height: 467px;
   margin: auto;
   width: 98%;

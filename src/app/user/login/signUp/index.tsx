ff7a13a95d2ca4dd91a64209b9e638299f898a5b
victor <victor_maanoel@outlook.com>
2023-12-29 00:14:54 -0400

    feat(styled tags): add `styled` tags for customize icons

--- src/app/user/login/signUp/index.tsx
+++ src/app/user/login/signUp/index.tsx
@@ -5,6 +5,10 @@ import { CardMain } from "../components/CardMain.component";
 import { InputDate } from "../components/InputDate.component";
 import { SmallLetters } from "../components/SmallLetters.component";
 import SocialMedias from "../components/SocialMedias.components";
+import {
+  StyledSocialIconsContainer,
+  StyledSocialMediasCard,
+} from "../components/styles/cardSocialMedias.style";
 
 const SOCIAL_MIDIAS_LOGIN = "Or use your email for register";
 
@@ -13,11 +17,14 @@ export default function SignUp() {
     <>
       <CardHeader text={"Create Account"} />
 
-      <div>
-        <SmallLetters text={SOCIAL_MIDIAS_LOGIN} />
-        <SocialMedias picture="/images/facebook.png" alt="facebook icon" />
-        <SocialMedias picture="/images/googlesearch.png" alt="google icon" />
-      </div>
+      <StyledSocialMediasCard>
+        <SmallLetters text={SOCIAL_MIDIAS_LOGIN} alignBaseline={true} />
+        <StyledSocialIconsContainer>
+          <SocialMedias picture="/images/facebook.png" alt="facebook icon" />
+          <SocialMedias picture="/images/googlesearch.png" alt="google icon" />
+        </StyledSocialIconsContainer>
+      </StyledSocialMediasCard>
+
       <CardMain gap={"0.2rem"}>
         <InputDate type="text" placeHolder="Name" />
         <InputDate type="email" placeHolder="Email" />

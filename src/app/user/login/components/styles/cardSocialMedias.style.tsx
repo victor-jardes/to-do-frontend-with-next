fe84c88f60c4536e5c0e0d6c82631f96a504440b
victor <victor_maanoel@outlook.com>
2023-12-29 00:10:17 -0400

    feat(card icons): this file use `css` now

--- src/app/user/login/components/styles/cardSocialMedias.style.tsx
+++ src/app/user/login/components/styles/cardSocialMedias.style.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import Image from "next/image";
+import Link from "next/link";
 
 export const StyledSocialMediasCard = styled.div`
-  background-color: red;
   display: grid;
-  grid-template-rows: 30px 1fr;
+  grid-template-rows: 35px 1fr;
   height: 100px;
   width: 100%;
 `;
@@ -12,8 +12,16 @@ export const StyledSocialMediasCard = styled.div`
 export const StyledSocialIconsContainer = styled.section`
   display: flex;
   flex-direction: row;
-  gap: 10px;
+  gap: 3px;
   justify-content: center;
+`;
+
+export const StyledSocialLink = styled(Link)`
+  align-self: start;
+  border: 2px solid rgba(241, 240, 240, 0.975);
+  border-radius: 50%;
+  padding: 3px;
+  margin: 3px;
 `;
 
 export const StyledImgIcons = styled(Image)``;

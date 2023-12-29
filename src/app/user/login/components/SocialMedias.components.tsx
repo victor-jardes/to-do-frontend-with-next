9777c651e322063aaa36aea679da9a1325779ef4
victor <victor_maanoel@outlook.com>
2023-12-29 00:11:42 -0400

    refactor(styled tags): refactor for use `styled` tags now

--- src/app/user/login/components/SocialMedias.components.tsx
+++ src/app/user/login/components/SocialMedias.components.tsx
@@ -1,6 +1,8 @@
-import { StyledSocialLink } from "./styles/socialMedias.style";
 import { ISocialMediasComponentProps } from "@/types/ISocialMediasComponentProps";
-import { StyledImgIcons } from "./styles/cardSocialMedias.style";
+import {
+  StyledImgIcons,
+  StyledSocialLink,
+} from "./styles/cardSocialMedias.style";
 
 export default function SocialMedias({
   picture,
@@ -8,8 +10,8 @@ export default function SocialMedias({
 }: ISocialMediasComponentProps) {
   return (
     <>
-      <StyledSocialLink>
-        <StyledImgIcons src={picture} alt={alt} width={35} height={35} />
+      <StyledSocialLink href="#">
+        <StyledImgIcons src={picture} alt={alt} width={28} height={28} />
       </StyledSocialLink>
     </>
   );

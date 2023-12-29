73a824a89eeabc7749c1fd277f2f4cd9d0273c8f
victor <victor_maanoel@outlook.com>
2023-12-28 23:41:33 -0400

    refactor(tag): user `styled` tag

--- src/app/user/login/components/SocialMedias.components.tsx
+++ src/app/user/login/components/SocialMedias.components.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import { StyledSocialLink } from "./styles/socialMedias.style";
 import { ISocialMediasComponentProps } from "@/types/ISocialMediasComponentProps";
+import { StyledImgIcons } from "./styles/cardSocialMedias.style";
 
 export default function SocialMedias({
   picture,
@@ -9,7 +9,7 @@ export default function SocialMedias({
   return (
     <>
       <StyledSocialLink>
-        <Image src={picture} alt={alt} width={35} height={35} />
+        <StyledImgIcons src={picture} alt={alt} width={35} height={35} />
       </StyledSocialLink>
     </>
   );

601180862ca8e9cb2db4724b4fb9606338f608d3
victor <victor_maanoel@outlook.com>
2024-01-03 09:33:26 -0400

    feat(inputsdate.style): add css for visialize when to focus

--- src/app/user/login/components/styles/inputsDate.style.tsx
+++ src/app/user/login/components/styles/inputsDate.style.tsx
@@ -1,9 +1,18 @@
 import styled from "styled-components";
 
 export const StyledInputsDate = styled.input`
-  background: #d9d9d9;
+  background-color: #f0f0f0;
   border-radius: 10px;
+  font-size: 2rem;
   margin-bottom: 5px;
-  height: 5.5rem;
+  height: 4.5rem;
   width: 90%;
+  border: 2px solid black;
+
+  &:focus,
+  &:hover {
+    background: #e2e2e2;
+    border: 3px solid black;
+    width: 91.5%;
+  }
 `;

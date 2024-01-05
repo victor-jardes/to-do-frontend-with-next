d0ffea86ff60b6d0dd84575bb875d1f3bd3a2c15
victor <victor_maanoel@outlook.com>
2024-01-04 20:00:34 -0400

    feat(cardfooter): remove uncessary tag and add `css` for childrens

--- src/app/user/login/components/CardFooter.component.tsx
+++ src/app/user/login/components/CardFooter.component.tsx
@@ -1,17 +1,9 @@
-import { StyledCentelizerChildrens } from "../ui/styles/userInfosContainer.style";
 import { StyledCardFooter } from "./styles/cardFooter.style";
 
 interface ICardFooter {
-  gap: string;
   children: React.ReactNode;
 }
 
-export function CardFooter({ gap, children }: ICardFooter) {
-  return (
-    <StyledCardFooter>
-      <StyledCentelizerChildrens $gap={gap}>
-        {children}
-      </StyledCentelizerChildrens>
-    </StyledCardFooter>
-  );
+export function CardFooter({ children }: ICardFooter) {
+  return <StyledCardFooter>{children}</StyledCardFooter>;
 }
--- src/app/user/login/components/styles/cardFooter.style.tsx
+++ src/app/user/login/components/styles/cardFooter.style.tsx
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const StyledCardFooter = styled.section``;
+export const StyledCardFooter = styled.section<{ $gap?: string }>`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.$gap || "20px"};
+  justify-content: center;
+`;

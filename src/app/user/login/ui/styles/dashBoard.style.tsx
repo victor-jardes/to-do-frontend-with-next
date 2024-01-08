d9721039ccce028da534de8a123ac4292e0d64a1
victor <victor_maanoel@outlook.com>
2024-01-08 19:56:03 -0400

    feat(dashboard.style): change display type, and add position for centilize div and remove margin

--- src/app/user/login/ui/styles/dashBoard.style.tsx
+++ src/app/user/login/ui/styles/dashBoard.style.tsx
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 export const StyledDashBoard = styled.div`
-  display: grid;
+  display: flex;
   background: #f0f0f0;
   border-radius: 10px;
   border-color: black;
   border-style: solid;
-  grid-template-columns: repeat(2, 1fr);
+  flex-direction: column;
   height: 500px;
-  margin: auto;
-  margin-top: 50px;
-  width: 900px;
+  left: 50%;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 460px;
 `;

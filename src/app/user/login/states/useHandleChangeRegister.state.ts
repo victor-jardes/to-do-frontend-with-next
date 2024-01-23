49f17696d92e35c8fda1aa431fa8a5eec2c56589
victor <victor_maanoel@outlook.com>
2024-01-23 16:39:08 -0400

    refactor(usehandlechangeregister.state.ts): add state for altanernet `login` and `register` page

--- src/app/user/login/states/useHandleChangeRegister.state.ts
+++ src/app/user/login/states/useHandleChangeRegister.state.ts
@@ -2,15 +2,23 @@ import { create } from "zustand";
 
 type State = {
   selectLogin: boolean;
+  signInDisabled: boolean;
+  signUpDisabled: boolean;
 };
 
 type Action = {
   setIsLogin: (selectLogin: boolean) => void;
+  setsignInDisabled: (signInDisabled: boolean) => void;
+  setsignUpDisabled: (signUpDisabled: boolean) => void;
 };
 
 const useHandleChangeRegister = create<State & Action>()((set) => ({
   selectLogin: true,
   setIsLogin: (selectLogin) => set(() => ({ selectLogin })),
+  signInDisabled: true,
+  setsignInDisabled: (signInDisabled) => set(() => ({ signInDisabled })),
+  signUpDisabled: false,
+  setsignUpDisabled: (signUpDisabled) => set(() => ({ signUpDisabled })),
 }));
 
 export default useHandleChangeRegister;
